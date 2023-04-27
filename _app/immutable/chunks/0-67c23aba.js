69d9630bb29ea3f6cdf06bfee189a19e12a4d8b1
Kolja <lasar@rasal.de>
2023-04-27 14:15:45 +0200

    Updates

---
+++ _app/immutable/chunks/0-67c23aba.js
@@ -0,0 +1,1 @@
+import{_ as r}from"./_layout-1c440c32.js";import{default as t}from"../components/pages/_layout.svelte-216d42ce.js";export{t as component,r as universal};
---
+++ _app/immutable/chunks/1-393d3922.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-a04c4179.js";export{t as component};
